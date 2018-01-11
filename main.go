f241f59a68399c937a537db445a028964830a268
aromero <aromero@AROMERO>
2018-01-11 08:37:07 -0500

    c3

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ const version string = "2.0.1"
 
 // VersionHandler handles incoming requests to /version
 // and just returns a simple version number
+// c3
 func versionHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, version)
 }
